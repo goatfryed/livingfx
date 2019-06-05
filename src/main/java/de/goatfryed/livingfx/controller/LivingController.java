783af2482f8c52b120ec3ab8fe635c76bfeb0fd4
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-05 22:31:19 +0200

    Added default empty behaviour

--- src/main/java/de/goatfryed/livingfx/controller/LivingController.java
+++ src/main/java/de/goatfryed/livingfx/controller/LivingController.java
@@ -1,4 +1,23 @@
 package de.goatfryed.livingfx.controller;
 
 public interface LivingController extends RootAware {
+
+    default boolean shouldComponentUpdate() {
+        return true;
+    }
+
+    default void componentDidUpdate() {
+    }
+
+    default void componentDidMount() {
+
+    }
+
+    default void componentWillUnmount() {
+
+    }
+
+    default void componentDidUnmount() {
+
+    }
 }
