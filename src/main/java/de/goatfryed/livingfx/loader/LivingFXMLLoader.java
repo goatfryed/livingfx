86c51663b4e66cad0242002faf31950b2a704faa
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-06 01:05:56 +0200

    Changed LivingFXMLLoader to facade

--- src/main/java/de/goatfryed/livingfx/loader/LivingFXMLLoader.java
+++ src/main/java/de/goatfryed/livingfx/loader/LivingFXMLLoader.java
@@ -1,56 +1,28 @@
 package de.goatfryed.livingfx.loader;
 
 import javafx.fxml.FXMLLoader;
-import javafx.util.BuilderFactory;
-import javafx.util.Callback;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-public class LivingFXMLLoader extends FXMLLoader {
+public class LivingFXMLLoader {
     private final Set<LoadPostProcessor> postProcessors = new LinkedHashSet<>();
 
+    private final FXMLLoader wrapped;
+
     public LivingFXMLLoader() {
-    }
-
-    public LivingFXMLLoader(URL location) {
-        super(location);
-    }
-
-    public LivingFXMLLoader(URL location, ResourceBundle resources) {
-        super(location, resources);
-    }
-
-    public LivingFXMLLoader(URL location, ResourceBundle resources, BuilderFactory builderFactory) {
-        super(location, resources, builderFactory);
-    }
-
-    public LivingFXMLLoader(URL location, ResourceBundle resources, BuilderFactory builderFactory, Callback<Class<?>, Object> controllerFactory) {
-        super(location, resources, builderFactory, controllerFactory);
-    }
-
-    public LivingFXMLLoader(Charset charset) {
-        super(charset);
-    }
-
-    public LivingFXMLLoader(URL location, ResourceBundle resources, BuilderFactory builderFactory, Callback<Class<?>, Object> controllerFactory, Charset charset) {
-        super(location, resources, builderFactory, controllerFactory, charset);
-    }
-
-    public LivingFXMLLoader(URL location, ResourceBundle resources, BuilderFactory builderFactory, Callback<Class<?>, Object> controllerFactory, Charset charset, LinkedList<FXMLLoader> loaders) {
-        super(location, resources, builderFactory, controllerFactory, charset, loaders);
+        wrapped = new FXMLLoader();
     }
 
     /**
      * @throws FXMLLoadingException in case of an IOException at loading.
      */
-    @Override
     public <T> T load() {
         try {
-            final T loaded = super.load();
+            final T loaded = wrapped.load();
             onLoaded();
             return loaded;
         } catch (IOException e) {
@@ -59,10 +31,9 @@ public class LivingFXMLLoader extends FXMLLoader {
 
     }
 
-    @Override
     public <T> T load(InputStream inputStream) {
         try {
-            final T load = super.load(inputStream);
+            final T load = wrapped.load(inputStream);
             onLoaded();
             return load;
         } catch (IOException e) {
@@ -80,5 +51,17 @@ public class LivingFXMLLoader extends FXMLLoader {
 
     public boolean addPostProcessor(LoadPostProcessor processor) {
         return postProcessors.add(processor);
+    }
+
+    public <T> T getRoot() {
+        return wrapped.getRoot();
+    }
+
+    public URL getLocation() {
+        return wrapped.getLocation();
+    }
+
+    public <T> T getController() {
+        return wrapped.getController();
     }
 }
